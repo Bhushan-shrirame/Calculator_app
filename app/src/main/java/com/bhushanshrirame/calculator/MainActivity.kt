1e8284c841be36de251f94eaeab184662656ef56
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-01-01 09:55:53 +0530

    First version

---
+++ app/src/main/java/com/bhushanshrirame/calculator/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.bhushanshrirame.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.inflate
+import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
+import com.bhushanshrirame.calculator.databinding.ActivityMainBinding
+import com.bhushanshrirame.calculator.databinding.ActivityMainBinding.inflate
+import com.bhushanshrirame.calculator.databinding.ActivitySplashScreenBinding.inflate
+import net.objecthunter.exp4j.ExpressionBuilder
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAc.setOnClickListener {
+            binding.InputText.text =""
+            binding.OutputText.text =""
+
+        }
+        binding.btn0.setOnClickListener {
+            binding.InputText.append("0")
+        }
+        binding.btn1.setOnClickListener {
+            binding.InputText.append("1")
+        }
+        binding.btn2.setOnClickListener {
+            binding.InputText.append("2")
+        }
+        binding.btn3.setOnClickListener {
+            binding.InputText.append("3")
+        }
+        binding.btn4.setOnClickListener {
+            binding.InputText.append("4")
+        }
+        binding.btn5.setOnClickListener {
+            binding.InputText.append("5")
+        }
+        binding.btn6.setOnClickListener {
+            binding.InputText.append("6")
+        }
+        binding.btn7.setOnClickListener {
+            binding.InputText.append("7")
+        }
+        binding.btn8.setOnClickListener {
+            binding.InputText.append("8")
+        }
+        binding.btn9.setOnClickListener {
+            binding.InputText.append("9")
+        }
+
+        binding.btnPlus.setOnClickListener {
+            binding.InputText.append("+")
+        }
+        binding.btnMultiplication.setOnClickListener {
+            binding.InputText.append("*")
+        }
+        binding.btnMinus.setOnClickListener {
+            binding.InputText.append("-")
+        }
+        binding.btnDivide.setOnClickListener {
+            binding.InputText.append("/")
+        }
+
+        binding.btnDot.setOnClickListener {
+            binding.InputText.append(".")
+        }
+        binding.btnStartBracket.setOnClickListener {
+            binding.InputText.append("(")
+        }
+        binding.btnEndBracket.setOnClickListener {
+            binding.InputText.append(")")
+        }
+
+        binding.btnEqual.setOnClickListener {
+            val expression = ExpressionBuilder(binding.InputText.text.toString()).build()
+            val result = expression.evaluate()
+            val longresult =result.toLong()
+
+            if(result==longresult.toDouble()){
+                binding.OutputText.text = longresult.toString()
+
+            }else{
+                binding.OutputText.text = result.toString()
+            }
+        }
+
+    }
+}
